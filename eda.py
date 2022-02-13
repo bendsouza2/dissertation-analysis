51d43eadd3574388f21575d00a71ab66812c8b9f
bendsouza <bendsouza2@gmail.com>
2022-02-13 18:58:47 +0000

    Aggregated data for google and meta owned apps

--- eda.py
+++ eda.py
@@ -8,6 +8,16 @@ df = df.set_index('company')
 # Adding columns on how many categories of data companies collect
 total = df.sum(axis=1)
 df['total_collected'] = total
+print(df)
+
+# Aggregating the data collected by big tech companies
+meta_agg = df.loc(axis=0)['facebook', 'instagram', 'whatsapp'].sum()
+meta_agg.name = 'meta'
+google_agg = df.loc(axis=0)['google_maps', 'youtube', 'google_docs', 'google_sheets', 'gmail'].sum()
+google_agg.name = 'google'
+df = df.append(meta_agg)
+df = df.append(google_agg)
+print(df)
 
 max_poss = len(df.columns) - 1
 percentage = total/max_poss * 100
@@ -19,5 +29,6 @@ print(df.head())
 companies_collecting = df.sum(axis=0)
 print(companies_collecting)
 correlation = df.corr()
-print(correlation.between(0.7, 0.999))
+
+
 
