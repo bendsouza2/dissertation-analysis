f004fcda8e707f3a751bfa74420e1ab92565afe0
bendsouza <bendsouza2@gmail.com>
2022-02-15 14:01:10 +0000

    Created new dataframe merging MAU data with number of categories of personal data collected

--- eda.py
+++ eda.py
@@ -4,11 +4,11 @@ import pandas as pd
 df = pd.read_csv('binary_data.csv')
 df = df.set_index('company')
 
+MAU_df = pd.read_csv('MAU.csv', header=None)
+MAU_df.columns = ['company', 'MAU_millions', 'secondary_source']
+MAU_df = MAU_df.set_index('company')
+MAU_df['MAU_millions'] = MAU_df['MAU_millions'].astype(float)
 
-# Adding columns on how many categories of data companies collect
-total = df.sum(axis=1)
-df['total_collected'] = total
-print(df)
 
 # Aggregating the data collected by big tech companies
 meta_agg = df.loc(axis=0)['facebook', 'instagram', 'whatsapp'].sum()
@@ -17,13 +17,29 @@ google_agg = df.loc(axis=0)['google_maps', 'youtube', 'google_docs', 'google_she
 google_agg.name = 'google'
 df = df.append(meta_agg)
 df = df.append(google_agg)
-print(df)
+
+
+# Adding columns on how many categories of data companies collect
+total = df.sum(axis=1)
+df['total_collected'] = total
+
 
 max_poss = len(df.columns) - 1
 percentage = total/max_poss * 100
 df['percentage'] = percentage
 df['percentage'] = df['percentage'].round(decimals=2)
-print(df.head())
+
+
+# Merging the two dataframes
+comb = df[['percentage', 'total_collected']].merge(MAU_df, left_on='company', right_on='company')
+comb = comb.drop('secondary_source', axis=1)
+comb.dropna(axis=0, inplace=True)
+
+# Correlation between MAU and data collection
+monopoly_advantage = comb.corr()
+print(comb.dtypes)
+print(monopoly_advantage)
+
 
 # Summary statistics
 companies_collecting = df.sum(axis=0)
