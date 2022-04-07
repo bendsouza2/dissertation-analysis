c26a93ecfde26650e9e9b5004f9a1ef7e17c3f5e
bendsouza <bendsouza2@gmail.com>
2022-04-07 18:35:26 +0100

    Dropped unnecessary columns and read final table to csv file

--- pers.py
+++ pers.py
@@ -2,6 +2,18 @@ import pandas as pd
 
 initial = pd.read_csv('pd_final.csv')
 
+# Removing unnecessary columns
+unnecessary = ['marital_status', 'height', 'weight', 'next_of_kin', 'mum_maiden_name', 'birth_country',
+               'allergies']
+initial.drop(unnecessary, inplace=True, axis=1)
+
+# Merging columns where categories overlap
+initial.loc[initial['job'] == 1, 'employment_status'] = 1
+initial.loc[initial['employer'] == 1, 'employment_status'] = 1
+initial.loc[initial['past_employer'] == 1, 'employment_status'] = 1
+extra_job = ['job', 'employer', 'past_employer']
+initial.drop(extra_job, axis=1, inplace=True)
+
 # Adding column of how many categories of data each company collects
 total = initial.sum(axis=1)
 initial['total_collected'] = total
@@ -9,4 +21,6 @@ maxi = len(initial.columns)
 percentage = total/maxi * 100
 initial['percentage_collected'] = percentage.round(decimals=2)
 print(initial)
+
+initial.to_csv('final_personal_data.csv')
 
