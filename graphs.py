1e0743f16e03294cd196a319108d27b8bf0dc792
bendsouza <bendsouza2@gmail.com>
2022-05-03 17:06:44 +0100

    Created figure 3

--- graphs.py
+++ graphs.py
@@ -12,10 +12,24 @@ spl = make_interp_spline(one_x, one_y, k=7)
 power_smooth = spl(xnew)
 
 fig5 = plt.plot(xnew, power_smooth)
-plt.title('Figure 5')
-plt.xlabel('Output')
+plt.title('Figure 4')
+plt.xlabel('Users')
 plt.ylabel('Average Cost')
 frame1 = plt.gca()
 frame1.axes.get_xaxis().set_ticks([])
 frame1.axes.get_yaxis().set_ticks([])
+# plt.show()
+
+# Abuse of dominance cases
+reason = ['Preferential \nTreatment', 'Preadtory Pricing', 'Illegal Tying', 'Restricting Access \nto Data']
+num = [7, 5, 4, 3]
+
+plt.bar(reason, num)
+fig, ax = plt.subplots()
+plt.title('Figure 3 - Practices Investigated in Abuse of Dominance Cases')
+plt.xlabel('Reason')
+plt.ylabel('Total')
+bars = ax.barh(reason, num)
+ax.bar_label(bars, label_type='center')
+fig.set_size_inches(12, 4.5)
 plt.show()
